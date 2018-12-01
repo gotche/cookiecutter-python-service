3fa899a89b1a8eeb13726b096f7cca3f39a1801e
Jose Martin <j.martin.bejarano@gmail.com>
2018-12-02 00:04:03 +0100

    Add support for encrypted vars

--- {{cookiecutter.project_slug}}/tools/install_dev_env.sh
+++ {{cookiecutter.project_slug}}/tools/install_dev_env.sh
@@ -22,3 +22,19 @@ pip install -r requirements-dev.txt
 
 # Install pre-commit hooks using the pre-commit framework
 pre-commit install
+
+# Use direnv for sensitive env vars
+if ! [ -x "$(command -v direnv)" ]; then
+    echo "Please install direnv: sudo apt install direnv"
+    exit 1
+fi
+
+if [[ -z "${ANSIBLE_VAULT_PASSWORD_FILE}" ]]; then
+    echo "Please set the team vault password in a file in "
+    echo "your filesystem and then add this to your .bashrc"
+    echo "export ANSIBLE_VAULT_PASSWORD_FILE=~/.ansible_vault.pass"
+    echo "Don't forget to populate the pass file"
+    exit 1
+fi
+
+direnv allow
