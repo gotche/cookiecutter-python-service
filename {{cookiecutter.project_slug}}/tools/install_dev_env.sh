91aee485ea79c2fd099fc3c1133aae6925a617da
Jose Martin <j.martin.bejarano@gmail.com>
2018-12-01 22:13:14 +0100

    Add creation of virtualenv as part of the creation of dev env

---
+++ {{cookiecutter.project_slug}}/tools/install_dev_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Use pyenv to handle virtual envs
+if ! [ -x "$(command -v pyenv)" ]; then
+    echo "Please install pyenv: https://github.com/pyenv/pyenv#installation"
+    exit 1
+fi
+
+if [ -e .python-version ]; then
+    echo "Found .python-version. Using it as virtualenv"
+else
+    echo "Creating a virtualenv"
+    pyenv virtualenv -p python3.7 3.7.1 {{cookiecutter.project_slug}}
+    echo "Activating virtualenv"
+    pyenv local {{cookiecutter.project_slug}}
+fi
+
+# Install dev requirements
+pip install -r requirements-dev.txt
+
+# Install pre-commit hooks using the pre-commit framework
+pre-commit install
